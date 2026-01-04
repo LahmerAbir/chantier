847eadd039dfb99e70e874d60599db3d4c0fa1d8
Lahmer Abir <abirlahmer16@gmail.com>
2026-01-04 10:31:37 +0100

    ++

--- lib/blocs/login_form_bloc.dart
+++ lib/blocs/login_form_bloc.dart
@@ -1,83 +1,110 @@
 import 'dart:convert';
-
- 
-import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../repository/auth_repository.dart';
 import '../utils/utils.dart';
 
+// États du formulaire
+abstract class LoginFormState extends Equatable {
+  const LoginFormState();
+  
+  @override
+  List<Object?> get props => [];
+}
 
-class LoginFormBloc extends FormBloc<String, String> {
+class LoginFormInitial extends LoginFormState {}
+
+class LoginFormLoading extends LoginFormState {}
+
+class LoginFormSuccess extends LoginFormState {}
+
+class LoginFormFailure extends LoginFormState {
+  final String error;
+  const LoginFormFailure(this.error);
+  
+  @override
+  List<Object?> get props => [error];
+}
+
+// Cubit pour gérer la logique
+class LoginFormBloc extends Cubit<LoginFormState> {
   final AuthRepository authRepository;
-  final username = TextFieldBloc(
-    validators: [
-      Utils.required,
-     // _checkEmail
-    ],
-  );
+  
+  // On garde les valeurs ici (similaire aux TextEditingController mais dans le bloc)
+  // Idéalement, on passerait ces valeurs via des events ou state, mais pour une migration simple :
+  String username = '';
+  String password = '';
+  bool stayConnect = true;
 
-  final password = TextFieldBloc(
-    validators: [
-      Utils.required,
-    ],
-  );
-
-  final stayConnect = BooleanFieldBloc(initialValue: true);
   LoginFormBloc({
     required this.authRepository,
-  }) {
-    addFieldBlocs(
-      fieldBlocs: [
-        username,
-        password,
-        stayConnect,
-      ],
-    );
+  }) : super(LoginFormInitial());
 
+  void updateUsername(String value) {
+    username = value;
   }
 
+  void updatePassword(String value) {
+    password = value;
+  }
+  
+  void updateStayConnect(bool value) {
+    stayConnect = value;
+  }
 
+  // Méthode appelée pour pré-remplir (depuis initState de la page)
+  void setInitialValues({required String initialUsername, required String initialPassword}) {
+    username = initialUsername;
+    password = initialPassword;
+  }
 
+  Future<void> submit() async {
+    if (username.isEmpty || password.isEmpty) {
+      emit(const LoginFormFailure("Veuillez remplir tous les champs"));
+      // Retour à initial pour permettre de réessayer sans bloquer l'UI
+      emit(LoginFormInitial()); 
+      return;
+    }
 
-  @override
-  void onSubmitting() async {
-    emitLoading();
-    SharedPreferences pref = await SharedPreferences.getInstance();
+    emit(LoginFormLoading());
+    
+    try {
+      SharedPreferences pref = await SharedPreferences.getInstance();
+      pref.setBool('stay_connect', stayConnect);
 
-    pref.setBool('stay_connect', stayConnect.value);
+      var response = await authRepository.login(username, password);
+      
+      if (response != null) {
+        Utils.isFirstAccess = null;
 
-    try{
-   var response = await authRepository.login(username.value, password.value);
-    print(response.toString());
-    if (response != null) {
-      Utils.isFirstAccess = null;
-
-      if (stayConnect.value == true) {
-        await authRepository.stayConnect(response);
-        Utils.setMailUser(username.value);
-        Utils.setPasswordUser(password.value);
+        if (stayConnect) {
+          await authRepository.stayConnect(response);
+          Utils.setMailUser(username);
+          Utils.setPasswordUser(password);
+        } else {
+          Utils.setMailUser(null);
+          Utils.setPasswordUser(null);
+        }
+        
+        var me = await Utils.getMeFromShared();
+        if (me != null) {
+          Utils.isFirstCnx(true);
+          Utils.setMe(me);
+          Utils.setIdUser(me.id);
+          pref.setString("me", jsonEncode(me.toJson()));
+        }
+        
+        emit(LoginFormSuccess());
       } else {
-        Utils.setMailUser(null);
-        Utils.setPasswordUser(null);
+        emit(const LoginFormFailure("Identifiants incorrects"));
+        emit(LoginFormInitial());
       }
-      var me = await Utils.getMeFromShared();
-      if (me != null) {
-        Utils.isFirstCnx(true);
-        SharedPreferences pref = await SharedPreferences.getInstance();
-        Utils.setMe(me);
-        Utils.setIdUser(me.id);
-        pref.setString("me", jsonEncode(me.toJson()));
-      }
-      emitSuccess();
-    }  else {
-    print("emitFailure");
-    emitFailure();
+    } catch (e) {
+      print("Login exception: $e");
+      emit(const LoginFormFailure("Erreur de connexion"));
+      emit(LoginFormInitial());
     }
-    }catch(e){
-      print("exception $e");
-      emitFailure();
-    }
-
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,29 +1,24 @@
 import 'package:auto_route/annotations.dart';
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_bloc/flutter_form_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_units/responsive_units.dart';
+import 'package:auto_route/auto_route.dart';
 
 import '../blocs/login_form_bloc.dart';
-
 import '../repository/auth_repository.dart';
 import '../resources/colors.dart';
 import '../resources/images.dart';
-import '../resources/strings.dart';
-import '../resources/styles.dart';
 import '../router/app_router.dart';
-import 'package:auto_route/auto_route.dart';
 
 import '../ui/common/button.dart';
 import '../ui/common/loading_dialog.dart';
 import '../ui/common/row_spacer.dart';
-import '../ui/common/text_button.dart';
-import '../ui/common/text_control.dart';
 import '../ui/common/unfocusKeybord.dart';
 import '../utils/utils.dart';
-import 'package:flutter/foundation.dart';
+
 @RoutePage()
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key, this.onSuccess, this.padding = true})
@@ -37,35 +32,44 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String? mail;
-  String? password;
+  // Controleurs pour les champs texte
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  
   bool isFirst = true;
+  bool _isPasswordVisible = false;
 
   @override
   void initState() {
+    super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       var email = await Utils.getMailUser();
       var pwd = await Utils.getPasswordlUser();
-      setState(() {
-        mail = email;
-        password = pwd;
-      });
+      
+      if (email != null) _usernameController.text = email;
+      if (pwd != null) _passwordController.text = pwd;
+      
+      // Mise à jour de l'état local pour rafraîchir l'UI si nécessaire
+      if (mounted) setState(() {});
     });
-    super.initState();
   }
-  double? horizontalPadding;
+  
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
+  double? horizontalPadding;
 
   @override
   Widget build(BuildContext context) {
-
-    return UnfocusKeyboard(
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        resizeToAvoidBottomInset: true,
-        body: Container(
-        decoration:   BoxDecoration(
-          ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: UnfocusKeyboard(
+        child: Container(
           height: MediaQuery.of(context).size.height,
           child: loginForm(context),
         ),
@@ -77,210 +81,202 @@ class _LoginPageState extends State<LoginPage> {
     var authRepository = context.read<AuthRepository>();
     final screenWidth = MediaQuery.of(context).size.width;
     final screenheight = MediaQuery.of(context).size.height;
-    // Détection de la plateforme d'exécution
+
     if (defaultTargetPlatform == TargetPlatform.android ||
         defaultTargetPlatform == TargetPlatform.iOS) {
-      horizontalPadding =  9;
+      horizontalPadding = 9;
+    } else {
+      horizontalPadding = screenWidth * 0.35;
     }
-    else
-      {
-        horizontalPadding =  screenWidth*0.35;
-
-
-      }
+    
     return BlocProvider(
-      create: (context) => LoginFormBloc(authRepository: authRepository),
-      child: Builder(
-        builder: (context) {
-          final loginFormBloc = context.read<LoginFormBloc>();
-          if (isFirst) {
-            if (mail != null && password != null) {
-              if (mail!.isNotEmpty && password!.isNotEmpty) {
-                isFirst = false;
-                loginFormBloc.password.updateInitialValue(password!);
-                loginFormBloc.username.updateInitialValue(mail!);
-              }
-            }
-          }
-          return FormBlocListener<LoginFormBloc, String, String>(
-            onLoading: (context, state) {
-              LoadingDialog.show(context, root: false);
-            },
-            onSuccess: (context, state) async {
-              LoadingDialog.show(context, root: false);
-
-              widget.onSuccess?.call();
-              Utils.getFromPreference();
-              Utils.checkIsfirstCnx();
-              Utils.isFirstCnx(true);
-              LoadingDialog.hide(context);
-           context.router.replaceAll([HomeRoute()]);
-              return;
-            },
-            onFailure: (context, state) {
-              print("");
-              LoadingDialog.hide(context);
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  backgroundColor: Colors.red,
-                  content: Text('Identifiants non correctes'),
-                ),
-              );
-
-              // Navigator.of(context, rootNavigator: true).pop();
-            },
-            child: SingleChildScrollView(
-              padding: EdgeInsets.symmetric(horizontal: horizontalPadding! , vertical: defaultTargetPlatform == TargetPlatform.android ||
-                  defaultTargetPlatform == TargetPlatform.iOS ? 15 : screenheight *0.1),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Hero(
-                        tag: 'logo_hero_tag', // Tag UNiQUE pour la transition
-                      child: Image(
-                        image: AssetImage(Utils.getImagePath(DeliveryImage.logo)),
-                        height: 200,
-                        width: 200,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 200,
-                    width: 200,
-                    child: TextFieldBlocBuilder(
-                      textFieldBloc: loginFormBloc.username,
-                      decoration: _inputDecoration(hintText: "Email"),
-                      suffixButton: SuffixButton.asyncValidating,
-
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(25.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          "Bienvenue",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 36,
-                            color: DeliveryColors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  _form(context, loginFormBloc),
-                ],
+      create: (context) {
+        final bloc = LoginFormBloc(authRepository: authRepository);
+        // Initialiser le bloc avec les valeurs récupérées si disponibles
+        if (_usernameController.text.isNotEmpty) {
+          bloc.updateUsername(_usernameController.text);
+          bloc.updatePassword(_passwordController.text);
+        }
+        return bloc;
+      },
+      child: BlocConsumer<LoginFormBloc, LoginFormState>(
+        listener: (context, state) {
+          if (state is LoginFormLoading) {
+            LoadingDialog.show(context, root: false);
+          } else if (state is LoginFormSuccess) {
+            LoadingDialog.hide(context);
+            widget.onSuccess?.call();
+            Utils.getFromPreference();
+            Utils.checkIsfirstCnx();
+            Utils.isFirstCnx(true);
+            context.router.replaceAll([HomeRoute()]);
+          } else if (state is LoginFormFailure) {
+            LoadingDialog.hide(context);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                backgroundColor: Colors.red,
+                content: Text(state.error),
               ),
+            );
+          }
+        },
+        builder: (context, state) {
+          final loginBloc = context.read<LoginFormBloc>();
+          
+          // Mise à jour des valeurs du bloc à chaque changement de texte
+          // On peut aussi le faire via onChanged dans les champs
+          
+          return SingleChildScrollView(
+            padding: EdgeInsets.symmetric(
+                horizontal: horizontalPadding!,
+                vertical: defaultTargetPlatform == TargetPlatform.android ||
+                        defaultTargetPlatform == TargetPlatform.iOS
+                    ? 15
+                    : screenheight * 0.1),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Hero(
+                    tag: 'logo_hero_tag',
+                    child: Image(
+                      image: AssetImage(Utils.getImagePath(DeliveryImage.logo)),
+                      height: 200,
+                      width: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Bienvenue",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36,
+                          color: DeliveryColors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                _form(context, loginBloc),
+              ],
             ),
           );
         },
       ),
     );
   }
-}
-InputDecoration _inputDecoration({
-  required String hintText,
-  Widget? suffixIcon,
-}) {
-  return InputDecoration(
-    hintText: hintText,
-    suffixIcon: suffixIcon,
-    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-    filled: true,
-    fillColor: Colors.grey.shade50,
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
-      borderSide: BorderSide.none,
-    ),
-    focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
-      borderSide: BorderSide(color: Colors.blue.shade700, width: 1.5),
-    ),
-  );
-}
 
-Widget _form(BuildContext context, LoginFormBloc loginFormBloc) {
+  InputDecoration _inputDecoration({
+    required String hintText,
+    Widget? suffixIcon,
+  }) {
+    return InputDecoration(
+      hintText: hintText,
+      suffixIcon: suffixIcon,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      filled: true,
+      fillColor: Colors.white,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: Colors.grey.shade300),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: Colors.grey.shade300),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: Colors.blue.shade700, width: 1.5),
+      ),
+    );
+  }
 
-
-  return SizedBox(
-    height: MediaQuery.of(context).size.height *0.5,
-    child: Column(
-        children: [
-          TextFieldBlocBuilder(
-            textFieldBloc: loginFormBloc.username,
-            decoration: _inputDecoration(hintText: "Email"),
-            suffixButton: SuffixButton.asyncValidating,
-
-          ),
-        /*  TextFormControl(
-            textFieldBloc: loginFormBloc.username,
-            suffixButton: SuffixButton.asyncValidating,
-            keyboardType: TextInputType.emailAddress,
-            label: "Email",
-
-          ),*/
-          TextFieldBlocBuilder(
-            textFieldBloc: loginFormBloc.password,
-            decoration: _inputDecoration(hintText: "Mot de passe" ,
+  Widget _form(BuildContext context, LoginFormBloc loginFormBloc) {
+    return Column(
+      children: [
+        TextFormField(
+          controller: _usernameController,
+          keyboardType: TextInputType.emailAddress,
+          style: const TextStyle(color: Colors.black),
+          decoration: _inputDecoration(hintText: "Email"),
+          onChanged: (value) => loginFormBloc.updateUsername(value),
+        ),
+        const SizedBox(height: 15),
+        TextFormField(
+          controller: _passwordController,
+          obscureText: !_isPasswordVisible,
+          style: const TextStyle(color: Colors.black),
+          decoration: _inputDecoration(
+            hintText: "Mot de passe",
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                color: Colors.grey,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isPasswordVisible = !_isPasswordVisible;
+                });
+              },
             ),
-            suffixButton: SuffixButton.obscureText,
-            obscureText: true,
-
           ),
-
-          GestureDetector(
-            onTap: () {
-              loginFormBloc.stayConnect.updateValue(
-                !loginFormBloc.stayConnect.value,
-              );
-            },
+          onChanged: (value) => loginFormBloc.updatePassword(value),
+        ),
+        GestureDetector(
+          onTap: () {
+            // Mettre à jour l'état local et le bloc
+            setState(() {
+              loginFormBloc.updateStayConnect(!loginFormBloc.stayConnect);
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  width: 150.sp,
-                  child: CheckboxFieldBlocBuilder(
-                    padding: const EdgeInsets.only(left: 0, right: 0),
-                    overlayColor: MaterialStateProperty.all<Color>(
-                      Colors.black,
-                    ),
-                    fillColor: MaterialStateProperty.all<Color>(Colors.grey),
-                    checkColor: MaterialStateProperty.all<Color>(Colors.black),
-                    side: BorderSide(color: Colors.black),
-                    textColor: MaterialStateProperty.all<Color>(Colors.black),
-                    booleanFieldBloc: loginFormBloc.stayConnect,
-                    controlAffinity: FieldBlocBuilderControlAffinity.leading,
-                    body: Padding(
-                      padding: const EdgeInsets.only(left: 0.0),
-                      child: Text(
-                        'Rester connecté',
-                        style: GoogleFonts.inter(fontSize: 10),
-                      ),
-                    ),
+                SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: Checkbox(
+                    value: loginFormBloc.stayConnect,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        loginFormBloc.updateStayConnect(value ?? false);
+                      });
+                    },
+                    checkColor: Colors.white,
+                    activeColor: Colors.black,
                   ),
                 ),
-
+                const SizedBox(width: 8),
+                Text(
+                  'Rester connecté',
+                  style: GoogleFonts.inter(fontSize: 12, color: Colors.black),
+                ),
               ],
             ),
           ),
-          RowSpacer(),
-          FormButton(
-            labelColor: Colors.white,
-            width: double.infinity,
-            onPressed: () async {
-              loginFormBloc.submit();
-            },
-            primary: DeliveryColors.gray,
-            radius: 15,
-            label: 'Connexion',
-          ),
-
-          RowSpacer(),
-        ],
-      ),
-  );
+        ),
+        const SizedBox(height: 20),
+        FormButton(
+          labelColor: Colors.white,
+          width: double.infinity,
+          onPressed: () {
+            // Mise à jour finale avant submit pour être sûr
+            loginFormBloc.updateUsername(_usernameController.text);
+            loginFormBloc.updatePassword(_passwordController.text);
+            loginFormBloc.submit();
+          },
+          primary: DeliveryColors.gray,
+          radius: 15,
+          label: 'Connexion',
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
 }
